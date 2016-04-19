fca04e4ced64dfacfd26e1155591f36467e05982
zc1415926 <zc1415926@126.com>
2016-04-19 13:02:04 +0800

    学生管理页添加页头

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -10,33 +10,35 @@ var StuInfoModal = require('./student/studentInfoModal');
 
 var HomePage = React.createClass({
 
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             students: StudentStore.getAllStudents(),
             modalIsOpen: false
         };
     },
 
-    componentDidMount: function() {
+    componentDidMount: function () {
         StudentStore.addChangeListener(this._onChange);
     },
 
-    componentWillUnmount: function() {
+    componentWillUnmount: function () {
         StudentStore.removeChangeListener(this._onChange);
     },
 
-    _onChange: function() {
-        this.setState({ students: StudentStore.getAllStudents() });
+    _onChange: function () {
+        this.setState({students: StudentStore.getAllStudents()});
     },
 
-    openModal: function(stu,event) {
-        this.setState({currentStudent: stu,
-            modalIsOpen: true});
+    openModal: function (stu, event) {
+        this.setState({
+            currentStudent: stu,
+            modalIsOpen: true
+        });
     },
 
     render: function () {
 
-        var createAuthorRow = function(student) {
+        var createAuthorRow = function (student) {
             return (
                 <tr key={student.id}>
                     <td>{student.student_number}</td>
@@ -46,7 +48,7 @@ var HomePage = React.createClass({
                     <td>{student.student_class}</td>
                     <td>
                         <button type="button" className="btn btn-link btn-student-operation"
-                            onClick={this.openModal.bind(this, student)}>
+                                onClick={this.openModal.bind(this, student)}>
                             <span className="glyphicon glyphicon-pencil"></span>
                         </button>
                         <button type="button" className="btn btn-link btn-student-operation">
@@ -59,23 +61,27 @@ var HomePage = React.createClass({
 
         return (
             <div className="container">
-                <h1>Hello!!</h1>
-                <Link to="/student" className="btn btn-primary">Add Student</Link>
+                <div className="jumbotron subPage">
+                    <h1>学生管理</h1>
+                    <p>您可以在这里添加、删除、修改学生信息。</p>
+                    <Link to="/student" className="btn btn-primary btn-lg">添加学生</Link>
+                </div>
+
                 <table className="table">
                     <thead>
-                        <tr>
-                            <th>学号</th>
-                            <th>姓名</th>
-                            <th>入学年</th>
-                            <th>年级</th>
-                            <th>班级</th>
-                            <th>操作</th>
-                        </tr>
+                    <tr>
+                        <th>学号</th>
+                        <th>姓名</th>
+                        <th>入学年</th>
+                        <th>年级</th>
+                        <th>班级</th>
+                        <th>操作</th>
+                    </tr>
                     </thead>
                     <tbody>{this.state.students.map(createAuthorRow, this)}</tbody>
                 </table>
 
-                <StuInfoModal isOpen={this.state.modalIsOpen} currentStudent={this.state.currentStudent} />
+                <StuInfoModal isOpen={this.state.modalIsOpen} currentStudent={this.state.currentStudent}/>
             </div>
         );
     }
