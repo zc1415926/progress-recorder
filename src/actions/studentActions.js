a9458fd5cfa18969b29c5db2e237005f5e14576a
zc1415926 <zc1415926@126.com>
2016-07-20 16:48:01 +0800

    使用axios替代jQuery进行ajax请求

--- src/actions/studentActions.js
+++ src/actions/studentActions.js
@@ -5,20 +5,24 @@
 
 var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../actions/actionTypes');
+var axios = require('axios');
 
 var env = require('../env.json');
 console.log('服务器端地址：' + env.SERVER_BASE_URL);
 
 var StudentAction = {
     createStudent: function () {
-        
+
     },
+
     getAllStudents: function () {
-        $.ajax({
-            type: 'GET',
-            url: env.SERVER_BASE_URL + '/student/index',
-            success: this.dispatchGetAllStudentsAction
-        });
+        axios.get(env.SERVER_BASE_URL + '/student/index')
+            .then(function(response){
+                dispatchGetAllStudentsAction(response['data']);
+            })
+            .catch(function(error){
+                console.log(error);
+            });
     },
     getStudentById: function () {
         
@@ -28,18 +32,14 @@ var StudentAction = {
     },
     deleteStudent: function () {
         
-    },
-
-    dispatchGetAllStudentsAction: function (data) {
-        //console.log("jQuery get data: ");
-        //console.log(data);
-        //console.log("jQuery out.");
-        
-        Dispatcher.dispatch({
-            actionType: ActionTypes.GET_ALL_STUDENTS,
-            students: data
-        });
     }
+};
+
+var dispatchGetAllStudentsAction = function (data) {
+    Dispatcher.dispatch({
+        actionType: ActionTypes.GET_ALL_STUDENTS,
+        students: data
+    });
 };
 
 module.exports = StudentAction;
