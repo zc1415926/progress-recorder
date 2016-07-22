6ac32f6c03a608d27067727a3ee993e210ea6669
zc1415926 <zc1415926@126.com>
2016-07-22 21:10:37 +0800

    使用navTab进行导航条优化

--- src/components/app/partials/nav.js
+++ src/components/app/partials/nav.js
@@ -4,16 +4,23 @@
 'use strict';
 
 var React = require('react');
-var Link = require('react-router').Link;
+var NavTab = require('./navTab');
 
 var Nav = React.createClass({
     render: function () {
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
-                    <Link to="home" className="navbar-brand">
-                        React
-                    </Link>
+                    <div className="navbar-header">
+                        <a className="navbar-brand">学生过程性评价系统</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <NavTab to="/">首页</NavTab>
+                            <NavTab to="/students">学生管理</NavTab>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         );
