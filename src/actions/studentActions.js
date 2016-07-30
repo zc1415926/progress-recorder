37a8ac5af881668bed4b09b7919c951039a254f0
zc1415926 <zc1415926@126.com>
2016-07-30 18:44:34 +0800

    添加studentActions思路注释

--- src/actions/studentActions.js
+++ src/actions/studentActions.js
@@ -16,6 +16,10 @@ var StudentAction = {
             data: stuObj
         })
             .then(function(response){
+                /*
+                如果成功添加了一个学生，则重新读取一次该年班学生，之后由getStudentsByGradeClass发射Dispatch
+                这样就可以在成功添加一个学生后，自动刷新该年班学生列表
+                 */
                 if(response['data']['status'] == "success"){
                     getStudentsByGradeClass(stuObj['student_grade'], stuObj['student_class'])
                 }else{
@@ -44,6 +48,10 @@ var StudentAction = {
     },
 
     getStudentsByGradeClass: function(gradeNum, classNum){
+        /*
+        将依据年级和班级查询学生的代码独立出去，因为在添加、更新、删除学生后，
+        也要使用这一部分代码刷新学生列表
+         */
         getStudentsByGradeClass(gradeNum, classNum);
     },
 
@@ -71,6 +79,11 @@ var StudentAction = {
 var getStudentsByGradeClass = function(gradeNum, classNum){
     axios.get(env.SERVER_BASE_URL + '/student/' + gradeNum + '/' + classNum)
         .then(function(response){
+            /*
+            在成功查询到学生信息后，发射dispatch向studentStore传送新的学生信息，
+            并使studentStore发生一个学生信息被修改事件，订阅了这一消息的组件，
+            收到消息后，就会从studentStore中读取新的学生信息，更新学生列表
+             */
             Dispatcher.dispatch({
                 actionType: ActionTypes.GET_STUDENTS_BY_GRADE_CLASS,
                 students: response['data']
