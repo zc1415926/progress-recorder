3ac08ca95d19d8572939309a52d7d5e357128e91
zc1415926 <zc1415926@126.com>
2016-07-30 17:21:01 +0800

    整理路由

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@
 //$ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var StudentActions = require('./actions/studentActions');
 
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
@@ -13,17 +12,12 @@ var IndexRoute = require('react-router').IndexRoute;
 
 var App = require('./components/app/app');
 var Home = require('./components/app/partials/homePage');
-var Students = require('./components/student/studentManager');
-var StudentCreate = require('./components/student/pages/studentCreatePage');
-
-
-//StudentActions.getAllStudents();
 
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Home}/>
-            <Route path="/students" component={Students}/>
+            <Route path="/students" component={require('./components/student/studentManager')}/>
             <Route path="/behaviour" component={require('./components/behaviour/behaviourPage')}/>
             <Route path="*" component={require('./components/app/pageNotFind')}/>
         </Route>
