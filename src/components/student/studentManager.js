5d94626b59445f3eb97a0f8f571f420ac1fc4bd0
zc1415926 <zc1415926@126.com>
2016-11-22 22:11:38 +0800

    简化openmodal

--- src/components/student/studentManager.js
+++ src/components/student/studentManager.js
@@ -28,53 +28,44 @@ var StudentManager = React.createClass({
 
     componentDidMount: function () {
         StudentStore.addEventListener(StudentStore.CHANGE_EVENT, this._onChange);
-        //GradeClassStore.addEventListener(GradeClassStore.GET_CLASS_CODE_EVENT, this.onGetClassCode);
     },
 
     componentWillUnmount: function () {
         StudentStore.removeEventListener(StudentStore.CHANGE_EVENT, this._onChange);
-        //GradeClassStore.removeEventListener(GradeClassStore.GET_CLASS_CODE_EVENT, this.onGetClassCode);
     },
 
     _onChange: function () {
         this.setState({students: StudentStore.getStudents()});
     },
 
-    /*onGetClassCode: function () {
-        this.setState({classCode: GradeClassStore.getClassCode()});
-    },*/
-
-    openStuCreateModal: function(){
-        this.setState({
-            isStuCreateModalOpen: true
-        });
-    },
-
-    openEditModal: function (stu, event) {
-        this.setState({
-            isEditModalOpen: true,
-
-            /*
-            如果不这样进行一次操作，传返回的stu的话，会导致这stu一直是“传引用（瞎猜的）”，
-            使得当在修改学生信息对话框中修改了文本框中的内容并点击“取消”后，
-            学生列表中的相应位置在内存中被修改，使用下面的代码打断引用的传递解决该问题
-             */
-            targetStudent: {
-                student_number      : stu['student_number'],
-                student_name        : stu['student_name'],
-                classCode  : GradeClassStore.getClassCode()}
-        });
-    },
-
-    openStuDelModal: function(stu, event){
-        this.setState({
-            targetStudent: stu,
-            isStuDelModalOpen: true
-        });
-    },
-
     onGradeClassSelected: function (gradeNum, classNum) {
         StudentActions.getStudentsByGradeClass(gradeNum, classNum);
+    },
+
+    openCrudModal: function (modalName, student) {
+        switch (modalName){
+            case 'create':
+                this.setState({isStuCreateModalOpen: true});
+                break;
+            case 'update':
+                this.setState({
+                    isEditModalOpen: true,
+                    /*
+                     如果不这样进行一次操作，传返回的stu的话，会导致这stu一直是“传引用（瞎猜的）”，
+                     使得当在修改学生信息对话框中修改了文本框中的内容并点击“取消”后，
+                     学生列表中的相应位置在内存中被修改，使用下面的代码打断引用的传递解决该问题
+                     */
+                    targetStudent: {
+                        student_number      : student['student_number'],
+                        student_name        : student['student_name'],
+                        classCode  : GradeClassStore.getClassCode()
+                    }
+                });
+                break;
+            case 'delete':
+                this.setState({isStuDelModalOpen: true,targetStudent: student});
+                break;
+        }
     },
 
     closeCrudModal: function (modalName) {
@@ -93,6 +84,11 @@ var StudentManager = React.createClass({
         this.setState({targetStudent: {}});
     },
 
+    onInputValueChanged: function (e) {
+        this.state.targetStudent[e.target.id] = e.target.value;
+        this.setState({targetStudent: this.state.targetStudent});
+    },
+
     render: function () {
         return (
             <div className="container">
@@ -103,23 +99,26 @@ var StudentManager = React.createClass({
                 </div>
 
                 <StudentList students={this.state.students}
-                             onCreateClick={this.openStuCreateModal}
-                             onEditClick={this.openEditModal}
-                             onDeleteClick={this.openStuDelModal}/>
+                             onCreateClick={this.openCrudModal.bind(null, 'create')}
+                             onEditClick={this.openCrudModal.bind(null, 'update')}
+                             onDeleteClick={this.openCrudModal.bind(null, 'delete')}/>
 
                 <CreateStudentModal isOpen={this.state.isStuCreateModalOpen}
                                     student={this.state.targetStudent}
                                     title={'添加学生信息'}
+                                    onInputValueChanged={this.onInputValueChanged}
                                     closeModal={this.closeCrudModal.bind(null, 'create')}/>
 
                 <UpdateStudentModal isOpen={this.state.isEditModalOpen}
                                     student={this.state.targetStudent}
                                     title={'修改学生信息'}
+                                    onInputValueChanged={this.onInputValueChanged}
                                     closeModal={this.closeCrudModal.bind(null, 'update')}/>
 
                 <DeleteStudentModal isOpen={this.state.isStuDelModalOpen}
                                     student={this.state.targetStudent}
                                     title={'删除学生信息'}
+                                    onInputValueChanged={this.onInputValueChanged}
                                     closeModal={this.closeCrudModal.bind(null, 'delete')}/>
             </div>
         );
