5f50caf0775c216ba8667f0b6fceffef4b69f8b3
zc1415926 <zc1415926@126.com>
2016-12-13 12:14:18 +0800

    use password input to input password

--- src/components/auth/authPage.js
+++ src/components/auth/authPage.js
@@ -5,6 +5,7 @@
 
 var React = require('react');
 var Input = require('../app/ui/inputGroup');
+var Password = require('../app/ui/passwordGroup');
 var authActions = require('../../actions/authActions');
 var AuthStore = require('../../stores/authStore');
 var browserHistory = require('react-router').browserHistory;
@@ -23,9 +24,6 @@ var AuthPage = React.createClass({
 
     componentDidMount: function () {
         AuthStore.addEventListener(AuthStore.AUTH_SUCCESS, this.onAuthSuccess);
-       // this.setState({to});
-        //初次打开页面，获取一次数据
-       // GradeClassActions.getGradeClasses();
     },
 
     componentWillUnmount: function () {
@@ -40,10 +38,7 @@ var AuthPage = React.createClass({
     },
 
     onModalChange:function(e){
-        //console.log(e.target.id);
-        //console.log(e.target.value);
         this.state.credential[e.target.id] = e.target.value;
-        //console.log(this.state.credential);
         return this.setState({credential:this.state.credential});
     },
 
@@ -57,7 +52,7 @@ var AuthPage = React.createClass({
                 <h1>登录</h1>
                 <form>
                     <Input id="email" text="用户名：" onChange={this.onModalChange}/>
-                    <Input id="password" text="密码：" onChange={this.onModalChange}/>
+                    <Password id="password" text="密码：" onChange={this.onModalChange}/>
                 </form>
                 <button className='btn btn-primary' onClick={this.onLoginClicked}>
                     登录
